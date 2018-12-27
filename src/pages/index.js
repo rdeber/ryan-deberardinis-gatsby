02f00f106a8d875f17969e3a45e4ccc6a28e2b9c
Ryan DeBerardinis <rdeber@gmail.com>
2018-12-27 14:56:20 -0500

    Testing css components

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,15 @@ import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import Image from '../components/image'
 import SEO from '../components/seo'
+import mystyles from '../styles/index.module.scss'
+import '../styles/main.scss'
+
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1 className={mystyles.container}>Hi people</h1>
+    <p className="container">Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
