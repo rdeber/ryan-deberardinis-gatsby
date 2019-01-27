0cd700fd526faae76e4cf0814df26be2e71b14b7
Ryan DeBerardinis <rdeber@gmail.com>
2019-01-26 20:34:32 -0500

    fix contact page animations

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,6 +6,9 @@ import { FiGithub } from 'react-icons/fi'
 import { FiInstagram } from 'react-icons/fi'
 import { FiCodepen } from 'react-icons/fi'
 import { FiImage } from 'react-icons/fi'
+import Reveal from 'react-reveal/Reveal';
+import Fade from 'react-reveal/Fade';
+
 
 const ContactPage = () => (
   <Layout>
@@ -17,18 +20,22 @@ const ContactPage = () => (
           <h2 className="my-positions">I'm always interested in hearing about new projects. Drop me a line if you'd like to collaborate on something new.</h2>
 
           <div className="external-links">
-            <a href="https://github.com/rdeber" className="tooltip tooltip-bottom" data-tooltip="View source on Github"><FiGithub /></a>
-            <a href="https://codepen.io/rdeber/" className="tooltip tooltip-bottom" data-tooltip="Check out my Codepens"><FiCodepen /></a>
-            <a href="https://www.instagram.com/r.deber/" className="tooltip tooltip-bottom" data-tooltip="Follow me on Instagram"><FiInstagram /></a>
-            <a href="https://www.shutterstock.com/g/deberarr" className="tooltip tooltip-bottom" data-tooltip="View my photos on Shutterstock"><FiImage /></a>
+            
+              <Fade delay={1000}><a href="https://github.com/rdeber" className="tooltip tooltip-bottom" data-tooltip="View source on Github"><FiGithub /></a></Fade>
+              <Fade delay={1200}><a href="https://codepen.io/rdeber/" className="tooltip tooltip-bottom" data-tooltip="Check out my Codepens"><FiCodepen /></a></Fade>
+              <Fade delay={1400}><a href="https://www.instagram.com/r.deber/" className="tooltip tooltip-bottom" data-tooltip="Follow me on Instagram"><FiInstagram /></a></Fade>
+              <Fade delay={1600}><a href="https://www.shutterstock.com/g/deberarr" className="tooltip tooltip-bottom" data-tooltip="View my photos on Shutterstock"><FiImage /></a></Fade>
+            
           </div>
           
         </div>
-        <div className="box-3d-right contact-form">
-          <div className="myForm">
-            <Contact />
+        <Reveal effect="fadeInRight3dBox">
+          <div className="box-3d-right contact-form">
+            <div className="myForm">
+              <Contact />
+            </div>
           </div>
-        </div>
+        </Reveal>
       </section>
     </div>
   </Layout>
